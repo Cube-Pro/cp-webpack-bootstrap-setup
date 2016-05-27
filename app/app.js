c09e9bff7ceac1f5b8a2ee68d917fe522479fcf9
CubePro <cubeprofessional@gmail.com>
2016-05-27 16:34:32 -0500

    First Commit

---
+++ app/app.js
@@ -0,0 +1,18 @@
+var jQuery = require("jquery");
+require("bootstrap-webpack");
+require("bootstrap-webpack!./bootstrap.config.js");
+require("./css/style.css");
+//require('jQuery');
+
+// var $ = require('jquery');
+
+// $(function () {
+//   var $header = $('#header');
+//   $('#nav').affix({
+//     offset: {
+//       top: function () {
+//         return $header.offset().top + $header.outerHeight(true);
+//       }
+//     }
+//   });
+// });
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require("webpack");
+
+// In webpack.config.js
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+module.exports = {
+  entry: [
+    path.resolve(__dirname, './app/app.js')
+  ],
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: "index.js"
+  },
+  module: {
+    loaders: [
+      //{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      // **IMPORTANT** This is needed so that each bootstrap js file required by
+      // bootstrap-webpack has access to the jQuery object
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+
+      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+      // loads bootstrap's css.
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+    ]
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery"
+    })
+  ]
+};
